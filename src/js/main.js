6e869371f77bfc99c4862e827fe7f3cddc8e0014
Sergio Ávila <sergio.avila.astorga@gmail.com>
2024-07-29 16:27:20 -0400

    add base bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,30 @@
 // Load Styles
 import '../scss/main.scss';
-
 // Load Bootstrap init
 import {initBootstrap} from "./bootstrap.js";
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
 
 // Loading bootstrap with optional features
 initBootstrap({
-  tooltip: true,
-  popover: true,
-  toasts: true,
+  tooltip: false,
+  popover: false,
+  toasts: false,
 });
 
-// Your app code
-console.log(`Hello ${process.env.HELLO}`);
+//init swiper 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
